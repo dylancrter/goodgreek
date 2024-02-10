a8a08cd24e7e5dfa92ee3f1621c5bc45baaa0e42
dylancrter <dylanc.rter@gmail.com>
2024-02-10 16:08:26 -0500

    "user"

--- ggbackend/src/main/java/net/goodgreek/ggbackend/GgbackendApplication.java
+++ ggbackend/src/main/java/net/goodgreek/ggbackend/GgbackendApplication.java
@@ -1,13 +1,29 @@
 package net.goodgreek.ggbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GgbackendApplication {
+import net.goodgreek.ggbackend.repository.UserRepository;
+import net.goodgreek.ggbackend.model.User;
 
-	public static void main(String[] args) {
-		SpringApplication.run(GgbackendApplication.class, args);
+@SpringBootApplication
+public class GgbackendApplication implements CommandLineRunner{
+
+	public static void main(String[] args) { SpringApplication.run(GgbackendApplication.class, args); }
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User();
+		user.setOrganization("Alpha Sigma Phi");
+		user.setEmail("dylanc.rter@gmail.com");
+		user.setName("Dylan Carter");
+		user.setPassword("DWCbaseball7$");
+		userRepository.save(user);
 	}
 
 }
--- ggbackend/src/main/java/net/goodgreek/ggbackend/repository/UserRepository.java
+++ ggbackend/src/main/java/net/goodgreek/ggbackend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package net.goodgreek.ggbackend.repository;
 
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import net.goodgreek.ggbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class UserRepository extends JpaRepositoriesAutoConfiguration{
-    
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // all crud database methods
 }
