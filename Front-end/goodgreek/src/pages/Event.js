c2d8bb7f66bed3942b2cd250cb2af89451b8eaa8
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2024-02-10 18:43:12 -0500

    Event and Eventlist Pull Data
    Event & Eventlist now *theoretically* pull data from the backend

--- Front-end/goodgreek/src/pages/Event.js
+++ Front-end/goodgreek/src/pages/Event.js
@@ -1,54 +1,118 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../css/App.css";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import '../css/event.css';
 import sample from '../assets/sampleposter.jpg';
 import axios from 'axios';
+import EventsService from "../services/EventsService";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
+var name;
+var org;
+var date;
+var location;
+var description;
+var price;
+var venmo ;
+var benefits;
+var charityweb;
 
 const events = [ {
     name: 'Feed the Hungry', datetime: 'Tomorrow', organizer: "Alpha Sig"
   },
 ];
 
+const test = () => {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <p>Organizer: {org}</p>
+      <p>Date: {date}</p>
+      <p>Location: {location}</p>
+
+      {price != null &&
+        <p>Price: {price}</p>
+      }
+      
+      {venmo != null &&
+        <p>Venmo: {venmo}</p>
+      }
+
+      {benefits != null &&
+        <p>Benefits: {benefits}</p>
+      }
+
+      {charityweb != null &&
+        <p>Charity Website: {charityweb}</p>
+      } 
+
+      <p>Description: {description}</p>
+    </div>
+  )
+}
+
+
 const Event = () =>{
-    const queryParameters = new URLSearchParams(window.location.search)
-    const name = queryParameters.get("name");
-    const org = queryParameters.get("org");
-    const date = queryParameters.get("date");
-    const location = queryParameters.get("location");
-    const description = queryParameters.get("description");
-    const price = queryParameters.get("price");
-    const venmo = queryParameters.get("venmo");
-    const benefits = queryParameters.get("benefits");
-    const charityweb = queryParameters.get("charityweb");
+    var queryParameters = new URLSearchParams(window.location.search)
+
+    const eventdata = () => {
+      EventsService.getAllEvents().then((response) => {
+        eventdata = Object.keys(response.data);
+        console.log(response.data);
+        eventdata.map(function(data) {
+          name = data.name;
+          org = data.organization;
+          date = data.date
+          location = data.location
+          description = data.description
+          price = data.price
+          venmo = data.venmo
+          benefits = data.benefits
+          charityweb = data.website
+          return (
+            <div>
+              <h1>Name: {name}</h1>
+              <p>Organizer: {org}</p>
+              <p>Date: {date}</p>
+              <p>Location: {location}</p>
+
+              {price != null &&
+                <p>Price: {price}</p>
+              }
+              
+              {venmo != null &&
+                <p>Venmo: {venmo}</p>
+              }
+
+              {benefits != null &&
+                <p>Benefits: {benefits}</p>
+              }
+
+              {charityweb != null &&
+                <p>Charity Website: {charityweb}</p>
+              } 
+
+              <p>Description: {description}</p>
+            </div>
+          )
+      })
+    })};
+
+    const id = queryParameters.get("id");
+
+    useEffect(() => {
+
+      EventsService.getEventById(id).then((response) => {
+        events = Object.keys(response.data);
+        console.log(response.data);
+      })
+
+  }, []);
 
     return (
       <div className="event">
-        <h1>Name: {name}</h1>
-        <p>Organizer: {org}</p>
-        <p>Date: {date}</p>
-        <p>Location: {location}</p>
-
-        {price != null &&
-          <p>Price: {price}</p>
-        }
-        
-        {venmo != null &&
-          <p>Venmo: {venmo}</p>
-        }
-
-        {benefits != null &&
-          <p>Benefits: {benefits}</p>
-        }
-
-        {charityweb != null &&
-          <p>Charity Website: {charityweb}</p>
-        } 
-
-        <p>Description: {description}</p>
-
+        {Event}
       </div>
     )
 }
