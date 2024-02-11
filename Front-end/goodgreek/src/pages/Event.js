dad5a561e1a1b9910911b7bc5dd41e0567bd93fb
John Medlock <jwmedlock@icloud.com>
2024-02-11 06:16:07 -0500

    added comments to Event.js

--- Front-end/goodgreek/src/pages/Event.js
+++ Front-end/goodgreek/src/pages/Event.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState, Image } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/App.css";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import '../css/event.css';
-import sample from '../assets/sampleposter.jpg';
-import axios from 'axios';
 import EventsService from "../services/EventsService";
 
 import image0 from '../assets/donation.png';
+import image3 from '../assets/hearthands.jpg';
 import image1 from '../assets/unity.jpg';
 import image2 from '../assets/volunteer.jpeg';
-import image3 from '../assets/hearthands.jpg';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Event = () => {
+  // State hooks for managing event data and loading state
   const [eventData, setEventData] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // Extracting 'id' from URL query parameters
   const id = new URLSearchParams(window.location.search).get("id");
 
+  // useEffect hook to fetch event data by ID on component mount or when 'id' changes
   useEffect(() => {
     try {
       EventsService.getEventById(id).then((response) => {
@@ -27,21 +27,25 @@ const Event = () => {
       });
     }
     catch(err) {
-      setLoading(false);
+      setLoading(false); // Ensure loading is set to false in case of an error
     }
     
-  }, [id]);
+  }, [id]); // Dependency array with 'id' to trigger effect when 'id' changes
 
+  // Conditional rendering for loading state
   if (loading) {
     return <div className="text">Loading...</div>;
   }
 
+  // Conditional rendering if no event data is found
   if (!eventData) {
     return <div className="text">No event found</div>;
   }
 
+  // Destructuring eventData for easier access to properties
   const { eventName, organization, date, location, eventDescription, price, venmo, benefits, website, image } = eventData;
 
+  // Rendering event details with conditional image display based on 'image' value
   return (
     <div className="event">
       {image == 0 && <img className="event-image" src={image0}/>}
@@ -64,4 +68,5 @@ const Event = () => {
   );
 };
 
+// Exporting the Event component for use in other parts of the application
 export default Event;
