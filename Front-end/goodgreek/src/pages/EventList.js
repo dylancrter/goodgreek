afcaa8505598a78292e12f4421d66459d7503459
James (Grant) Moore <107823094+jgrantmoore@users.noreply.github.com>
2024-02-11 02:37:44 -0500

    Actually Removed Text Decoration

--- Front-end/goodgreek/src/pages/EventList.js
+++ Front-end/goodgreek/src/pages/EventList.js
@@ -18,7 +18,7 @@ const EventList = () => {
   }, []);
 
   return (
-    <div className="event-list" style={{textDecoration: none}}>
+    <div className="event-list">
       {!sessionStorage.isLoggedIn && (
         <meta http-equiv="Refresh" content="0; url='/signin'" />
       )}
